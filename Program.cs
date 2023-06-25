31a9e81ef88efd1effe7138936575ead4c8e5278
Александр Щербаков <alex17.05.84@yandex.ru>
2023-06-25 11:36:36 +0300

    Задача решена

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿//  Напишите программу, которая принимает на вход координаты двух
+// точек и находит расстояние мужду ними в 2D пространстве.  
+// Формула расчета расстояния: d= √(x2 - x1) + (y2-y1)
+
+double GetDistance(double x1, double x2, double y1, double y2)
+{
+    double distance = Math.Sqrt(Math.Pow((x2 - x1),2) + Math.Pow((y2 - y1),2));
+    return distance;
+}
+
+double GetNumber(string name)
+{
+    Console.WriteLine("Введите координату " + name);
+    return Convert.ToDouble(Console.ReadLine());
+ }
+
+ double x1 = GetNumber("x1");
+ double x2 = GetNumber("x2");
+ double y1 = GetNumber("y1");
+ double y2 = GetNumber("y2");
+
+ Console.WriteLine("Расстояние между точками = " + GetDistance(x1, x2, y1, y2));
